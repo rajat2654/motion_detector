22216bd90273e2498db69df4efa9f39377a5d330
rajat2654 <rajat97533@gmail.com>
2019-08-31 12:58:05 +0530

    object time record

--- detect.py
+++ detect.py
@@ -1,13 +1,19 @@
 from cv2 import cv2
-import time
-video = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+import time, pandas
+from datetime import datetime as dt
+
+video = cv2.VideoCapture(0)
 frame_1 = None
+status = [None]
+times = []
+df=pandas.DataFrame(columns=["Start","End"])
 
 while (True):
     check, frame = video.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
-    
+    state = 0
+
     if frame_1 is None:
         frame_1 = gray
         continue
@@ -18,21 +24,36 @@ while (True):
     
     (cnts, _) = cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     for contour in cnts:
-        if cv2.contourArea(contour) < 1000:
+        if cv2.contourArea(contour) < 2000:
             continue
+        state = 1
         x, y, w, h = cv2.boundingRect(contour)
         cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 3)
-    
-    cv2.imshow("Gray", frame_1)
-    cv2.imshow("Delta", delta_frame)
+    status.append(state)
+
+    if status[-1] == 1 and status[-2] == 0:
+        times.append(dt.now())
+    elif status[-1] == 0 and status[-2] == 1:
+        times.append(dt.now())
+
+
+    #cv2.imshow("Gray", frame_1)
+    #cv2.imshow("Delta", delta_frame)
     cv2.imshow("Threshold", thresh_frame)
     cv2.imshow("Rectangles", frame)
     
-    
-    #print(delta_frame)
 
-    if cv2.waitKey(1) == ord('a'):
+    if cv2.waitKey(30) == ord('p'):
+        if state == 1:
+            times.append(dt.now())
         break
+#print(status)
+print(times)
+
+for i in range(0, len(times), 2):
+    df = df.append({"Start":times[i],"End":times[i+1]},ignore_index=True)
+#df.to_csv("times.csv")
+print(df)
 
 video.release()
 cv2.destroyAllWindows()

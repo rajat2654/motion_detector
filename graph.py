645c21e606745cd0faeddc0ea40943c46d304b9d
rajat2654 <rajat97533@gmail.com>
2019-09-03 00:15:28 +0530

    hover feature

--- graph.py
+++ graph.py
@@ -1,10 +1,19 @@
 from detect import df
 from bokeh.plotting import figure, show, output_file
+from bokeh.models import HoverTool, ColumnDataSource
+
+df["Start_text"] = df["Start"].dt.strftime("%Y-%m-%d %H:%M:%S")
+df["End_text"] = df["End"].dt.strftime("%Y-%m-%d %H:%M:%S")
+
+cds=ColumnDataSource(df)
 
 p = figure(x_axis_type='datetime', height=100, width=500, sizing_mode="scale_both", title="Motion Graph")
 p.yaxis.minor_tick_line_color = None
 p.ygrid[0].ticker.desired_num_ticks = 1
 
-q = p.quad(left=df["Start"], right=df["End"], bottom=0, top=1, color="green")
+hover = HoverTool(tooltips=[("Start", "@Start_text"), ("End", "@End_text")])
+p.add_tools(hover)
+
+q = p.quad(left="Start", right="End", bottom=0, top=1, color="green", source=cds)
 output_file("Graph.html")
 show(p)
